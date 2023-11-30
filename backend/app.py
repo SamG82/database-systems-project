2d1c1938a21892269557dad238046a2ff1f0d8ba
SamG82 <samg13579@gmail.com>
2023-11-30 15:25:30 -0500

    fabricating test data

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ import models
 import os
 from auth import auth_bp
 from api import api_bp
+from fabricate import fabricate_data
 
 # configuration and setup
 app = Flask(__name__)
@@ -12,11 +13,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 models.db.init_app(app)
 
-# reset databse on startup - debug
 with app.app_context():
     models.db.drop_all()
     models.db.create_all()
-
+    fabricate_data()
+    
 models.ma.init_app(app)
 
 app.register_blueprint(auth_bp)
