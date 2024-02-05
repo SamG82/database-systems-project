a304e09f7b8146a37a053e62acf6010e46b4958e
SamG82 <samg13579@gmail.com>
2024-02-04 20:13:31 -0500

    Tweaked prompt slightly

--- backend/api/services/gemini.py
+++ backend/api/services/gemini.py
@@ -3,29 +3,27 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-API_KEY = os.getenv('KEY_HERE')
+API_KEY = os.getenv('gemini_api_key')
 
 genai.configure(api_key=API_KEY)
-gen_config = genai.GenerationConfig(temperature=0)
+gen_config = genai.GenerationConfig(
+    candidate_count=1,
+    stop_sequences=['.'],
+    top_p = 0.7,
+    top_k = 4,
+    temperature=0)
+
+
 model = genai.GenerativeModel('gemini-pro')
 
 PROMPT_BASE = '''Your role is to function as a translation layer between everyday language 
                 and medical terminology. When a user inputs a description of their symptoms, 
                 you will convert this description into the precise medical terms used by healthcare professionals. 
-                Your responses should be concise and focused solely on the translation of terms.'''
+                Your responses should be concise and focused solely on the translation of these symptoms: '''
+
 
 def get_symptoms_suggestions(symptoms_text: str) -> str:
     prompt = PROMPT_BASE + symptoms_text
-    response = model.generate_content(prompt,
-                                  generation_config=genai.types.GenerationConfig(
-                                  candidate_count=1,
-                                  stop_sequences=['.'],
-                                  max_output_tokens=20,
-                                  top_p = 0.7,
-                                  top_k = 4,
-                                  temperature=0.7)
-                                  )
+    response = model.generate_content(prompt, generation_config=gen_config)
 
     return response.text
-
-    print(get_symptoms_suggestions("I am experiencing chest pain, sore throat and cough."))
